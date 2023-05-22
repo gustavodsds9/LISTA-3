d7585c7035876ceb2bba9672ed87e00d3da9cc5a
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-05-21 22:53:09 -0300

    Add files via upload

---
+++ EX1 Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1_LISTA_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 0;
+
+            Console.WriteLine("Lista 3, Ex 1:");
+
+            do
+            {
+                Console.WriteLine("Informe o Valor:");
+                valor = double.Parse(Console.ReadLine());
+            } 
+            while (valor < 0);
+            Console.WriteLine("Valor: {0} Aceito!",valor);
+        }
+    }
+}
